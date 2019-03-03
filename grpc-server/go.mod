a46aef0c0e232efb1b76b2aa8b504493ca81677a
Kazuhito Hokamura <k.hokamura@gmail.com>
2019-03-03 20:55:46 +0900

    Fetch data via db

--- grpc-server/go.mod
+++ grpc-server/go.mod
@@ -3,6 +3,7 @@ module github.com/adventar/adventar/grpc-server
 go 1.12
 
 require (
+	github.com/go-sql-driver/mysql v1.4.1
 	github.com/golang/protobuf v1.3.0
 	google.golang.org/grpc v1.19.0
 )
--- grpc-server/main.go
+++ grpc-server/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"net"
 
 	pb "github.com/adventar/adventar/grpc-server/adventar/v1"
+	_ "github.com/go-sql-driver/mysql"
 	"google.golang.org/grpc"
 )
 
@@ -13,13 +15,36 @@ const (
 	port = ":8080"
 )
 
+var db *sql.DB
+
 type server struct{}
 
+type Calendar struct {
+	ID          int64
+	UserID      int64
+	Title       string
+	Description string
+	Year        int32
+}
+
 func (s *server) GetCalendar(ctx context.Context, in *pb.GetCalendarRequest) (*pb.Calendar, error) {
-	return &pb.Calendar{Id: in.GetId()}, nil
+	var calendar Calendar
+	log.Printf("Request Id: %d", in.GetId())
+	err := db.QueryRow("select id, user_id, title, description, year from calendars where id = ?", in.GetId()).Scan(&calendar.ID, &calendar.UserID, &calendar.Title, &calendar.Description, &calendar.Year)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Calendar{Id: calendar.ID, UserId: calendar.UserID, Title: calendar.Title, Description: calendar.Description, Year: calendar.Year}, nil
 }
 
 func main() {
+	var err error
+	db, err = sql.Open("mysql", "root@tcp(127.0.0.1:3306)/adventar_dev")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	log.Printf("listening at %s\n", port)
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
