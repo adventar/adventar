38d7dc1fd4448949b40ba5c2c09df4cff905ab24
Kazuhito Hokamura <k.hokamura@gmail.com>
2019-03-31 21:46:39 +0900

    Set default timestamp

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE `calendars` (
   `title` text NOT NULL,
   `description` text NOT NULL,
   `year` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_calendars_user_id` FOREIGN KEY (user_id) REFERENCES users (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -19,8 +19,8 @@ CREATE TABLE `entries` (
   `url` text NOT NULL,
   `title` text NOT NULL,
   `image` text NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp,
   PRIMARY KEY (`id`),
   UNIQUE KEY `index_entries_on_calendar_id_and_date` (`calendar_id`,`date`) USING BTREE,
   CONSTRAINT `fk_entries_calendar_id` FOREIGN KEY (calendar_id) REFERENCES calendars (id) ON DELETE CASCADE,
@@ -33,8 +33,8 @@ CREATE TABLE `users` (
   `auth_uid` varchar(255) NOT NULL,
   `auth_provider` varchar(255) NOT NULL,
   `icon_url` text NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp,
   PRIMARY KEY (`id`),
   UNIQUE KEY `index_users_on_uid_and_provider` (`uid`,`provider`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
