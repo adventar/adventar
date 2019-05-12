0eb0f4aa4d3be16a0e40eb0402af60eb253dd2aa
Kazuhito Hokamura <k.hokamura@gmail.com>
2019-05-12 18:09:44 +0900

    Cleanup jest settings

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,14 +1,10 @@
 module.exports = {
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
-    "^~/(.*)$": "<rootDir>/$1",
-    "^vue$": "vue/dist/vue.common.js"
+    "^~/(.*)$": "<rootDir>/$1"
   },
-  moduleFileExtensions: ["js", "vue", "json"],
+  moduleFileExtensions: ["js", "ts", "vue", "json"],
   transform: {
-    "^.+\\.js$": "babel-jest",
-    ".*\\.(vue)$": "vue-jest"
-  },
-  collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/components/**/*.vue", "<rootDir>/pages/**/*.vue"]
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.vue$": "vue-jest"
+  }
 };
