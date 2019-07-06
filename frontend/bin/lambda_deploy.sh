28132329db915c6a382a2c8b7789f0bde2677256
Kazuhito Hokamura <k.hokamura@gmail.com>
2019-07-06 21:33:17 +0900

    Serverless!

---
+++ frontend/bin/lambda_deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+yarn install
+API_BASE_URL=https://grpc-dev.adventar.org yarn run build
+tsc --outDir .nuxt nuxt.config.ts
+rm -rf handler.zip
+zip handler.zip -rq lambda.js .nuxt node_modules
+envchain aws-personal aws s3 cp handler.zip s3://hokaccha/lambda/adventarDevSsr/handler.zip
+envchain aws-personal aws lambda update-function-code \
+  --function-name adventarDevSsr \
+  --s3-bucket hokaccha \
+  --s3-key lambda/adventarDevSsr/handler.zip
