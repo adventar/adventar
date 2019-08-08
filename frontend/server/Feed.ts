dee554df77f1cdb1bd8b13b9069926fa0a8c0da5
Kazuhito Hokamura <k.hokamura@gmail.com>
2019-08-08 20:17:07 +0900

    Implement feed

--- frontend/server/Feed.ts
+++ frontend/server/Feed.ts
@@ -8,8 +8,22 @@ async function generateCalendarFeed(calendarId: number): Promise<string> {
     title: `${calendar.title} Advent Calendar ${calendar.year}`,
     link: `https://adventar.org/calendars/${calendar.id}`,
     description: calendar.description,
+    generator: "Adventar",
     copyright: ""
   });
+
+  if (calendar && calendar.entries) {
+    calendar.entries.reverse().forEach(entry => {
+      if (!entry.url) return;
+      const description = `${calendar.title} Advent Calendar ${calendar.year} ${entry.day}日目`;
+      feed.addItem({
+        title: (entry.title || entry.comment || description).trim(),
+        description: description,
+        link: entry.url,
+        date: new Date(calendar.year, 11, entry.day)
+      });
+    });
+  }
 
   return feed.rss2();
 }
