275aca335a2b195ca92d8ece131678dd8860f0a5
Kazuhito Hokamura <k.hokamura@gmail.com>
2019-12-15 23:04:26 +0900

    Remove unused sg

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -45,20 +45,3 @@ resource "aws_security_group" "alb" {
     Name = "adventar-alb"
   }
 }
-
-resource "aws_security_group" "ssh" {
-  vpc_id      = aws_vpc.adventar.id
-  name        = "ssh"
-  description = "Allow ssh"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "adventar-ssh"
-  }
-}
