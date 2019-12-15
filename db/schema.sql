619f222b9348e1cbfcfe50cc731fb8184e84ab2d
Kazuhito Hokamura <k.hokamura@gmail.com>
2019-12-15 20:20:32 +0900

    Capitalize to avoid diff of schemalex

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE `users` (
   `auth_uid` varchar(255) NOT NULL,
   `auth_provider` varchar(20) NOT NULL,
   `icon_url` text NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp,
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `index_users_on_uid_and_provider` (`auth_uid`,`auth_provider`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -16,8 +16,8 @@ CREATE TABLE `calendars` (
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `year` smallint unsigned NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp,
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_calendars_user_id` FOREIGN KEY (user_id) REFERENCES users (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -31,8 +31,8 @@ CREATE TABLE `entries` (
   `url` text NOT NULL,
   `title` text NOT NULL,
   `image_url` text NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp,
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `index_entries_on_calendar_id_and_day` (`calendar_id`,`day`) USING BTREE,
   CONSTRAINT `fk_entries_calendar_id` FOREIGN KEY (calendar_id) REFERENCES calendars (id) ON DELETE CASCADE,
