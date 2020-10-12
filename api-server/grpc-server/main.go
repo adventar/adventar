8f07a96b8d4cd9e9ebb16c3ed7b51d0018d7b1c0
Kazuhito Hokamura <k.hokamura@gmail.com>
2020-10-12 12:41:38 +0900

    Add parseTime option for production

--- api-server/grpc-server/main.go
+++ api-server/grpc-server/main.go
@@ -16,6 +16,8 @@ func main() {
 	source := os.Getenv("DATABASE_SOURCE")
 	if source == "" {
 		source = "root@tcp(127.0.0.1:13306)/adventar_dev?parseTime=true"
+	} else {
+		source += "?parseTime=true"
 	}
 	db, err := sqlx.Open("mysql", source)
 	if err != nil {
