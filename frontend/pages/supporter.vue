1abfcede73b58894585e5f9fa9fbed0d3e5af9db
Kazuhito Hokamura <k.hokamura@gmail.com>
2022-11-30 19:25:06 +0900

    Remove cyboze from supporter

--- frontend/pages/supporter.vue
+++ frontend/pages/supporter.vue
@@ -4,18 +4,6 @@
     <main>
       <div class="mainInner">
         <section class="sectionItem">
-          <SectionHeader>サポーター</SectionHeader>
-          <ul class="supporters">
-            <li>
-              <a href="https://tech.cybozu.io/" rel="noopener noreferrer"
-                ><img src="~/assets/support/cybozu.png" alt="Cybozu" width="301" height="273" class="logoImg"
-              /></a>
-            </li>
-          </ul>
-        </section>
-      </div>
-      <div class="mainInner">
-        <section class="sectionItem">
           <SectionHeader>サポーターについて</SectionHeader>
           <p>
             Adventarは個人が趣味で運営しているサービスなのでこれまでもこれからも課金や広告などを導入することは考えていませんが、サーバーの運用費や開発費などをサポートしてくる企業や個人の方がおりましたら歓迎いたします。
