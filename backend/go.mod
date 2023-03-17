0cd99368c910b85839e94c08f5eaeb79830f0fd1
Kazuhito Hokamura <k.hokamura@gmail.com>
2023-03-17 16:47:23 +0900

    update

--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/bufbuild/connect-go v1.5.2
+	github.com/jmoiron/sqlx v1.3.5
 	golang.org/x/net v0.8.0
 	google.golang.org/protobuf v1.30.0
 )
--- backend/main.go
+++ backend/main.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	adventarv1 "github.com/adventar/adventar/backend/pkg/gen/adventar/v1"
 	"github.com/adventar/adventar/backend/pkg/gen/adventar/v1/adventarv1connect"
+	"github.com/adventar/adventar/backend/pkg/model"
 	"github.com/bufbuild/connect-go"
+	"github.com/jmoiron/sqlx"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
 type AdventarServer struct {
-	adventarv1connect.UnimplementedAdventarHandler // TODO: remove
+	// TODO: remove
+	adventarv1connect.UnimplementedAdventarHandler
+	db *sqlx.DB
 }
 
 func (s *AdventarServer) GetCalendar(
@@ -21,15 +27,23 @@ func (s *AdventarServer) GetCalendar(
 	req *connect.Request[adventarv1.GetCalendarRequest],
 ) (*connect.Response[adventarv1.GetCalendarResponse], error) {
 	id := req.Msg.CalendarId
+	calendar, err := getCalendar(s.db, id)
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
 	res := connect.NewResponse(&adventarv1.GetCalendarResponse{
 		Calendar: &adventarv1.Calendar{
-			Id: id,
+			Id:    calendar.ID,
+			Title: calendar.Title,
+			Year:  calendar.Year,
 		},
 	})
 	return res, nil
 }
 
 func main() {
+	db := setupDatabase()
+	defer db.Close()
 	mux := http.NewServeMux()
 	mux.Handle(adventarv1connect.NewAdventarHandler(&AdventarServer{}))
 	err := http.ListenAndServe(
@@ -37,4 +51,30 @@ func main() {
 		h2c.NewHandler(mux, &http2.Server{}),
 	)
 	log.Fatalf("listen failed: %v", err)
+}
+
+func setupDatabase() *sqlx.DB {
+	var err error
+	source := os.Getenv("DATABASE_SOURCE")
+	if source == "" {
+		source = "root@tcp(127.0.0.1:13306)/adventar_dev"
+	}
+	source += "?parseTime=true&charset=utf8mb4"
+	db, err := sqlx.Open("mysql", source)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func getCalendar(db *sqlx.DB, id int64) (*model.Calendar, error) {
+	query := "select id, title, year from calendars where id = ?"
+	var calendar model.Calendar
+	err := db.Get(&calendar, query, id)
+
+	if err != nil {
+		return nil, fmt.Errorf("error: %v", err)
+	}
+
+	return &calendar, nil
 }
